36261f360af11186f42a7ebb071bf7e3fe89b316
Pavlos Nicolaou <pauln1993@gmail.com>
2017-11-09 12:07:31 +0000

    buildin URI and showing an address on a the map using Intents

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -48,12 +48,14 @@ public class MainActivity extends AppCompatActivity {
      * @param v Button that was clicked.
      */
     public void onClickOpenAddressButton(View v) {
-        // TODO (5) Store an address in a String
-
-        // TODO (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
-
-        // TODO (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
-        Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
+        // DONE (5) Store an address in a String
+        String address = "3 Albion Place";
+        // Done (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
+        Uri uri = new Uri.Builder()
+                .scheme(address)
+                .build();
+        // DONE (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
+        showMap(uri);
     }
 
     /**
@@ -112,13 +114,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    // TODO (1) Create a method called showMap with a Uri as the single parameter
-    // Do steps 2 - 4 within the showMap method
-        // TODO (2) Create an Intent with action type, Intent.ACTION_VIEW
+    // DONE (1) Create a method called showMap with a Uri as the single parameter
+    void showMap(Uri uri) {
+        // Do steps 2 - 4 within the showMap method
+        // DONE (2) Create an Intent with action type, Intent.ACTION_VIEW
+        // DONE (3) Set the data of the Intent to the Uri passed into this method
+        // DONE (4) Verify that this Intent can be launched and then call startActivity
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.putExtra(Intent.EXTRA_STREAM, uri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
 
-        // TODO (3) Set the data of the Intent to the Uri passed into this method
-
-        // TODO (4) Verify that this Intent can be launched and then call startActivity
 
 
 }
