9eab1d475ee0f9699914b59c2711984f86f5512c
Pavlos Nicolaou <pauln1993@gmail.com>
2017-11-09 01:18:47 +0000

    creating intents

--- Lesson03-Green-Recycler-View/T03.07-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ Lesson03-Green-Recycler-View/T03.07-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -21,9 +21,10 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-// TODO (8) Implement GreenAdapter.ListItemClickListener from the MainActivity
-public class MainActivity extends AppCompatActivity {
+// DONE (8) Implement GreenAdapter.ListItemClickListener from the MainActivity
+public class MainActivity extends AppCompatActivity implements GreenAdapter.ListItemClickListener {
 
     private static final int NUM_LIST_ITEMS = 100;
 
@@ -34,7 +35,9 @@ public class MainActivity extends AppCompatActivity {
     private GreenAdapter mAdapter;
     private RecyclerView mNumbersList;
 
-    // TODO (9) Create a Toast variable called mToast to store the current Toast
+    // DONE (9) Create a Toast variable called mToast to store the current Toast
+    private Toast mToast;
+    private int mOnClickListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         mNumbersList = (RecyclerView) findViewById(R.id.rv_numbers);
 
         /*
-         * A LinearLayoutManager is responsible for measuring and positioning item views within a
+         * A  LinearLayoutManager is responsible for measuring and positioning item views within a
          * RecyclerView into a linear list. This means that it can produce either a horizontal or
          * vertical list depending on which parameter you pass in to the LinearLayoutManager
          * constructor. By default, if you don't specify an orientation, you get a vertical list.
@@ -67,11 +70,11 @@ public class MainActivity extends AppCompatActivity {
          */
         mNumbersList.setHasFixedSize(true);
 
-        // TODO (13) Pass in this as the ListItemClickListener to the GreenAdapter constructor
+        // DONE (13) Pass in this as the ListItemClickListener to the GreenAdapter constructor
         /*
          * The GreenAdapter is responsible for displaying each item in the list.
          */
-        mAdapter = new GreenAdapter(NUM_LIST_ITEMS);
+        mAdapter = new GreenAdapter(mOnClickListener, NUM_LIST_ITEMS);
         mNumbersList.setAdapter(mAdapter);
     }
 
@@ -94,8 +97,8 @@ public class MainActivity extends AppCompatActivity {
              * ways. (in our humble opinion)
              */
             case R.id.action_refresh:
-                // TODO (14) Pass in this as the ListItemClickListener to the GreenAdapter constructor
-                mAdapter = new GreenAdapter(NUM_LIST_ITEMS);
+                // DONE (14) Pass in this as the ListItemClickListener to the GreenAdapter constructor
+                mAdapter = new GreenAdapter(NUM_LIST_ITEMS, this );
                 mNumbersList.setAdapter(mAdapter);
                 return true;
         }
@@ -103,7 +106,16 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    // TODO (10) Override ListItemClickListener's onListItemClick method
-    // TODO (11) In the beginning of the method, cancel the Toast if it isn't null
-    // TODO (12) Show a Toast when an item is clicked, displaying that item number that was clicked
+    // DONE (10) Override ListItemClickListener's onListItemClick method
+    @Override
+    public void onListItemClick(int clickedItemIndex) {
+        // DONE (11) In the beginning of the method, cancel the Toast if it isn't null
+        // DONE (12) Show a Toast when an item is clicked, displaying that item number that was clicked
+
+        String toastMessage = "Item #" + clickedItemIndex + " clicked.";
+        mToast = Toast.makeText(this, toastMessage, Toast.LENGTH_LONG);
+
+        mToast.show();
+
+    }
 }
